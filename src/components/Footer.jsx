3e196f727f6977f6ea44432468faf2c71d9acddc
saddam-ansari <saddamansari426@gmail.com>
2023-01-03 16:41:32 +0530

    version 1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <h1>ReactStart</h1>
+                <p>@all right reserved</p>
+            </div>
+
+            <div>
+                <h5>Follow Us</h5>
+                <div>
+                    <a href="" target={"blank"}>Github</a>
+                    <a href="" target={"blank"}>Linkedin</a>
+                    <a href="" target={"blank"}>Instagram</a>
+                    <a href="" target={"blank"}>Facebook</a>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
